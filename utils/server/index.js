7287b6beeb7d65d9a9d1889748f002486e1c1126
Druw-Ilg <86981860+Druw-Ilg@users.noreply.github.com>
2023-07-30 23:39:34 +0200

    Update server index.js

--- utils/server/index.js
+++ utils/server/index.js
@@ -1,4 +1,4 @@
 // URLs for Development and Production environment
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "..";
+export const server = dev ? "http://localhost:3000" : "https://my-bank-six.vercel.app";
